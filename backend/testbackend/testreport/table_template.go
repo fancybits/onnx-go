3a758a2d8427c1080f0807b716de7d2756c6f3b5
Eric Lindvall <eric@5stops.com>
2026-01-05 22:38:43 -0800

    Update ONNX_COVERAGE.md

--- backend/testbackend/testreport/table_template.go
+++ backend/testbackend/testreport/table_template.go
@@ -7,8 +7,8 @@ var ReportTable = template.Must(template.New("report").Parse(ReportTemplate))
 
 // ReportTemplate ...
 const ReportTemplate = `
-|.| OpType      | Test                 | Tested | Skipped | Failed |
-|-|-------------|----------------------|--------|---------|--------|
+| Status | OpType | Test | Passed | Skipped | Failed |
+|:------:|--------|------|:------:|:-------:|:------:|
 {{ range . -}}
 |{{template "flag" .}}|{{ .OpType }}|{{ .Title }}|{{ .Tested }}|{{ .Skipped }}|{{ .Failed }}|
 {{ end }}
@@ -23,7 +23,7 @@ const ReportTemplate = `
 	{{- end -}}
     {{- end -}}
 {{- end -}}
-{{- define "red"}}![#f03c15](https://placehold.it/15/f03c15/000000?text=+){{ end }}
-{{- define "green"}}![#c5f015](https://placehold.it/15/c5f015/000000?text=+){{ end}}
-{{- define "orange"}}![#ffa500](https://placehold.it/15/FFA500/000000?text=+){{end}}
+{{- define "red"}}❌{{ end }}
+{{- define "green"}}✅{{ end}}
+{{- define "orange"}}⚠️{{end}}
 `
